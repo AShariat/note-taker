69b8139facc41e5586f33cfffc15b7cc21c8d3ef
Amir Shariat <amirshariat1987@gmail.com>
2022-03-09 00:57:12 -0700

    working on delete note function

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,27 @@
 const router = require('express').Router();
 
-const createNewNote = require('../../lib/notes.js');
+const { createNewNote, deleteNote } = require('../../lib/notes.js');
 const { notes } = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-  let results = notes;
   res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
   req.body.id = require('really-unique-id')();
-
   const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const { id } = req.body;
+  const deleted = notes.find(note => note.id === id);
+  let note;
+  if (deleted) {
+    note = deleteNote(id, notes);
+  } else {
+    res.status(404).json({ message: "Note you are looking for does not exist!"});
+  }
   res.json(note);
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
