9e24d198511b3c5f7186f5d0e4ddc13323b2848b
Amir Shariat <amirshariat1987@gmail.com>
2022-03-08 22:32:00 -0700

    added ID and post route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 
+const createNewNote = require('../../lib/notes.js');
 const { notes } = require('../../db/db.json');
+
+const randomId = require('../../lib/randomId.js');
 
 router.get('/notes', (req, res) => {
   let results = notes;
   res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = randomId;
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 });
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -10,4 +10,8 @@ router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
 module.exports = router;
